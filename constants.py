30313e7e2d80f6f38e26b56ccbc2d6ce27f80636
Soham Roy <sohamroy15@gmail.com>
2021-03-25 21:05:33 +0530

    Updated constants.py

--- constants.py
+++ constants.py
@@ -1,12 +1,15 @@
+#
+# define constants here
+#
 MIN_HIDDEN_LAYERS=1
 MAX_HIDDEN_LAYERS=4
 
-MIN_NODES=16
+MIN_NODES=8
 MAX_NODES=64
 
 SYNCH_STEP=5
 
-DIR_PATH='/home/wolfy/projects/fyp/datasets' #configure this on local repo
+DIR_PATH='/home/wolfy/projects/fyp/datasets' ###configure this on local repo
 
 TEST_SPLIT_FRACTION=0.2
 RANDOM_SAMPLE_LIMIT=5
