5919efb091eed2b4054345e7601f1386257bc88a
Soham Roy <sohamroy15@gmail.com>
2021-03-25 20:45:57 +0530

    Initial Commit

--- constants.py
+++ constants.py
@@ -3,10 +3,14 @@ MAX_HIDDEN_LAYERS=4
 
 MIN_NODES=16
 MAX_NODES=64
- 
+
 SYNCH_STEP=5
 
-DIR_PATH='/home/wolfy/projects/fyp/datasets'
+DIR_PATH='/home/wolfy/projects/fyp/datasets' #configure this on local repo
 
 TEST_SPLIT_FRACTION=0.2
-RANDOM_SAMPLE_LIMIT=5                                   
+RANDOM_SAMPLE_LIMIT=5
+
+NO_OF_AGENTS=5
+
+
