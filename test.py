8ce72a14f469ad75dbb235e99a33a87eff993663
Soham Roy <sohamroy15@gmail.com>
2021-03-25 21:08:47 +0530

    Random commit

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+#
+# random stuff goes here
+#
+
 def five(e):
-    return 5
+    return 5 + e
 def six(e):
-    return 6
+    return 6 + e
 
 sample = [six, five, six]
 func = [x(5) for x in sample]
