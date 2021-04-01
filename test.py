be305f2497145defdbd3edfca37522b5f12d464a
Sachinrocks <saharansachin98@gmail.com>
2021-04-01 11:37:48 +0000

    hi printed

--- test.py
+++ test.py
@@ -41,10 +41,11 @@ agent1.learn()
 '''
 from environment import Environment
 
-env = Environment()
+#env = Environment()
 
 #call multiAgent here
-controller = MADDPG(env, num_agents=C.NUM_AGENTS, alpha=C.ALPHA, beta=C.BETA, tau=C.TAU, input_dims=[C.NUM_AGENTS] ,n_actions=C.N_ACTIONS, hd1_dims = C.H1_DIMS, hd2_dims = C.H2_DIMS, mem_size = C.BUF_LEN,gamma = C.GAMMA, batch_size = C.BATCH_SIZE)
+#controller = MADDPG(env, num_agents=C.NUM_AGENTS, alpha=C.ALPHA, beta=C.BETA, tau=C.TAU, input_dims=[C.NUM_AGENTS] ,n_actions=C.N_ACTIONS, hd1_dims = #C.H1_DIMS, hd2_dims = C.H2_DIMS, mem_size = C.BUF_LEN,gamma = C.GAMMA, batch_size = C.BATCH_SIZE)
 
-controller.run()
+#controller.run()
+print("hi")
 
